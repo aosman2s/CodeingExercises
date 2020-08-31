b69394adc0f252e9c3321a4a73c42ba839ed3063
ahmed <ahmed.osama22@live.com>
2020-08-31 11:46:54 +0200

    Spring Basics : Springboot App and Autowiring

--- src/main/java/tarent/challenge/main/sorter/ArraySorter.java
+++ src/main/java/tarent/challenge/main/sorter/ArraySorter.java
@@ -2,12 +2,15 @@ package tarent.challenge.main.sorter;
 
 import java.util.Arrays;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ArraySorter {
-	
+
 	public int[] sort(int[] unsorted) {
 		int[] copy = Arrays.copyOf(unsorted, unsorted.length);
 		Arrays.sort(copy);
 		return copy;
-		}
+	}
 
 }
--- src/test/java/tarent/challenge/main/sorterTest/ArraySorterTest.java
+++ src/test/java/tarent/challenge/main/sorterTest/ArraySorterTest.java
@@ -6,17 +6,23 @@ import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tarent.challenge.main.sorter.ArraySorter;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { ArraySorter.class })
 public class ArraySorterTest {
 
-	private ArraySorter sorter;
+	@Autowired
+	private ArraySorter arraySorter;
 	private int[] unsortedArray;
 
 	@Before
 	public void setup() {
-		sorter = new ArraySorter();
 		Random random = new Random();
 		unsortedArray = new int[10];
 		for (int i = 0; i < unsortedArray.length; i++) {
@@ -26,7 +32,7 @@ public class ArraySorterTest {
 
 	@Test
 	public void testSort() {
-		int[] sorted = sorter.sort(unsortedArray);
+		int[] sorted = arraySorter.sort(unsortedArray);
 		for (int i = 0; i < sorted.length - 1; i++) {
 			assertTrue(sorted[i] <= sorted[i + 1]);
 		}
